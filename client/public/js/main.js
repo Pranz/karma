412fc5d8c980075f2321f0d6fe372ce92807060e
Jesper Fridefors <jesper.fridefors@gmail.com>
2016-11-26 09:07:29 +0100

    game loop and stuff

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -2,14 +2,45 @@
 window.onload = function() {
     var c = document.getElementById("game-screen");
     var ctx = c.getContext("2d");
-    while (true) {
-        var d = new Date();
-        var startTime = d.getTime();
-        ctx.fillRect(0,0,1400,800);
-        d = new Date();
-        var elapsedTime = startTime - d.getTime();
-        if (elapsedTime < 166) {
-            sleep(elapsedTime);
-        }
+    var state = getInitialState();
+    gameLoop(ctx, state)
+}
+
+function gameLoop(ctx, state) {
+    var d = new Date();
+    var startTime = d.getTime();
+
+    update(state);
+    render(ctx, state);
+
+    d = new Date();
+    var elapsedTime = startTime - d.getTime();
+    if (elapsedTime < 166) {
+        setTimeout(function() {
+            gameLoop(ctx, state);
+        }, 166 - elapsedTime);
+    } else {
+        gameLoop(ctx, state);
     }
+}
+
+function getInitialState() {
+    return {
+        player: {
+            x: 30,
+            y: 30
+        }
+    };
+}
+
+function update(state) {
+    state.player.x += 1;
+}
+
+function render(ctx, state) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,1400,800);
+    ctx.beginPath();
+    ctx.arc(state.player.x,state.player.y,50,0,Math.PI*2,true);
+    ctx.stroke();
 }
