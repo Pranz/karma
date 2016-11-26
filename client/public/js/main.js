f4911c1949d40484fe2eeb88afc88f31bcd9ae71
Jesper Fridefors <jesper.fridefors@gmail.com>
2016-11-26 09:23:26 +0100

    movement

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -3,7 +3,8 @@ window.onload = function() {
     var c = document.getElementById("game-screen");
     var ctx = c.getContext("2d");
     var state = getInitialState();
-    gameLoop(ctx, state)
+    document.addEventListener('keypress', function(event) { onKeyPress(state, event); });
+    gameLoop(ctx, state);
 }
 
 function gameLoop(ctx, state) {
@@ -15,10 +16,10 @@ function gameLoop(ctx, state) {
 
     d = new Date();
     var elapsedTime = startTime - d.getTime();
-    if (elapsedTime < 166) {
+    if (elapsedTime < 16) {
         setTimeout(function() {
             gameLoop(ctx, state);
-        }, 166 - elapsedTime);
+        }, 16 - elapsedTime);
     } else {
         gameLoop(ctx, state);
     }
@@ -34,12 +35,24 @@ function getInitialState() {
 }
 
 function update(state) {
-    state.player.x += 1;
+    
+}
+
+function onKeyPress(state, event) {
+    if (event.keyCode === 37) {
+        state.player.x -= 1;
+    } else if (event.keyCode === 38) {
+        state.player.y -= 1;
+    } else if (event.keyCode === 39) {
+        state.player.x += 1;
+    } else if (event.keyCode === 40) {
+        state.player.y += 1;
+    }
 }
 
 function render(ctx, state) {
     ctx.fillStyle = "white";
-    ctx.fillRect(0,0,1400,800);
+    ctx.fillRect(0,0,800,600);
     ctx.beginPath();
     ctx.arc(state.player.x,state.player.y,50,0,Math.PI*2,true);
     ctx.stroke();
